c9bd74ba09701068dcf824650077a449951abde1
allemand-instable <68656923+allemand-instable@users.noreply.github.com>
2021-02-22 16:33:02 +0100

    post

--- kill adobe.bat
+++ kill adobe.bat
@@ -1,3 +1,4 @@
+@echo off
 taskkill /F /IM "Adobe Desktop Service.exe"
 taskkill /F /IM "CCLibrary.exe"
 taskkill /F /IM "CCXProcess.exe"
@@ -7,3 +8,20 @@ taskkill /F /IM "CoreSync.exe"
 taskkill /F /IM "Creative Cloud Helper.exe"
 taskkill /F /IM "AdobeNotificationClient.exe"
 taskkill /F /IM "AcrobatNotificationClient.exe"
+taskkill /F /IM "AdobeARM.exe"
+
+sc stop AGSService
+sc stop AdobeUpdateService
+sc stop AGMService
+sc stop AdobeARMservice
+
+echo .
+echo .
+echo make sure to launch as administrator to disable services
+echo .
+echo .
+sc config AGSService start= disabled
+sc config AdobeUpdateService start= disabled
+sc config AGMService start= disabled
+sc config AdobeARMservice start= disabled
+PAUSE
